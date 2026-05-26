0cc837ea4b7c609d37755eee9f3a9de738ef4811
normanchingcuanco <norman.chingcuanco@gmail.com>
2026-05-26 19:30:24 +0800

    fix: update macro split to 25/45/30 industry standard

--- backend/utils/calorieCalculator.js
+++ backend/utils/calorieCalculator.js
@@ -1,5 +1,3 @@
-// TDEE Calculator using Mifflin-St Jeor formula
-
 const activityMultipliers = {
   sedentary: 1.2,
   light: 1.375,
@@ -9,23 +7,21 @@ const activityMultipliers = {
 };
 
 const calculateTDEE = ({ age, height, currentWeight, activityLevel, gender = 'male' }) => {
-  // BMR calculation
   let bmr;
   if (gender === 'female') {
     bmr = 10 * currentWeight + 6.25 * height - 5 * age - 161;
   } else {
     bmr = 10 * currentWeight + 6.25 * height - 5 * age + 5;
   }
-
   const multiplier = activityMultipliers[activityLevel] || 1.2;
   return Math.round(bmr * multiplier);
 };
 
 const calculateMacros = (calories) => {
   return {
-    protein: Math.round((calories * 0.30) / 4),  // 30% protein, 4 cal/g
-    carbs: Math.round((calories * 0.45) / 4),    // 45% carbs, 4 cal/g
-    fat: Math.round((calories * 0.25) / 9)       // 25% fat, 9 cal/g
+    protein: Math.round((calories * 0.25) / 4),
+    carbs: Math.round((calories * 0.45) / 4),
+    fat: Math.round((calories * 0.30) / 9)
   };
 };
 
